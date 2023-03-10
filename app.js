afd512779e715cf38a352a9a701a01662c502425
anthonyjeeng <somecodes56@gmail.com>
2023-03-10 12:41:24 -0500

    async await

--- app.js
+++ app.js
@@ -1,77 +1,19 @@
-const formulario = document.getElementById('formulario');
-const userName = document.getElementById('userName');
-const userEmail = document.getElementById('userEmail');
 
-const alertSuccess = document.getElementById('alertSuccess');
-const alertEmail = document.getElementById('alertEmail');
-const alertName = document.getElementById('alertName');
+const url = 'https://jsonplaceholder.typicode.com/posts/'
 
-const regUserName = /^[A-Za-zÑñÁáÉéÍíÓóÚúÜü\s]+$/;
-const regUserEmail = /^[a-z0-9]+(\.[_a-z0-9]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,15})$/;
+const findPostById = async (id) => {
 
-const pintarMensajeExito = () => {
+    try {
+        
+        const res = await fetch(url + id)
+        const post = await res.json()
 
-    alertSuccess.classList.remove('d-none');
-    alertSuccess.textContent = 'Mensaje enviado con éxito'
+        console.log(post)
 
-}
-
-const pintarMensajeError = (errores) => {
-
-    errores.forEach(item => {
-
-        item.tipo.classList.remove("d-none");
-        item.tipo.textContent = item.msg;
-
-    })
-
-}
-
-formulario.addEventListener('submit', e => {
-
-    e.preventDefault();
-
-    alertSuccess.classList.add('d-none');
-
-    const errores = [];
-
-    //esto devuelve true si existe solo espacios
-    //console.log(!userName.value.trim())
-
-    if (!regUserName.test(userName.value) || !userName.value.trim()) {
-
-        userName.classList.add("is-invalid")
-        errores.push({
-            tipo: alertName,
-            msg: "Formato no válido en el campo nombre, solo letras"
-        })
-
-    } else {
-        userName.classList.remove("is-invalid")
-        userName.classList.add("is-valid")
-        alertName.classList.add("d-none")
+    } catch (error) {
+        console.log(error)
     }
 
-    if (!regUserEmail.test(userEmail.value) || !userEmail.value.trim()) {
+};
 
-        userEmail.className.add("is-invalid")
-        errores.push({
-            tipo: alertEmail,
-            msg: "Escriba un correo válido"
-        })
-
-    } else {
-        userEmail.classList.remove("is-invalid");
-        userEmail.classList.add("is-valid");
-        alertEmail.classList.add("d-none");
-    }
-
-    if (errores.length !== 0){
-        pintarMensajeError(errores);
-        return;
-    }
-
-    console.log('formulario enviado');
-    pintarMensajeExito();
-
-})
+findPostById(3);
