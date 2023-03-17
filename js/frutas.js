c6931134519a0e2c8c7b30079cc1ca5da8e58286
anthonyjeeng <somecodes56@gmail.com>
2023-03-16 23:48:18 -0500

    ToDo listo

---
+++ js/frutas.js
@@ -0,0 +1,11 @@
+export const sandia = "sandia";
+
+export function pintarPlatano() {
+    console.log("platano")
+}
+
+export class Fruta {
+    constructor(nombre){
+        this.nombre = nombre;
+    }
+}
